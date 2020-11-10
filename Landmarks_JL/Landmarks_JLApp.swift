6c39daeffcbd70599b99628d464446e7daa95b26
Julio Lopez <Juliol2540@gmail.com>
2020-11-09 22:20:39 -0600

    Initial Commit

---
+++ Landmarks_JL/Landmarks_JLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_JLApp.swift
+//  Landmarks_JL
+//
+//  Created by Julio Lopez on 11/9/20.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_JLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
